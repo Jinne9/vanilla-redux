921b77acb3b40bc34c00969b8fa3d1584ebb0f46
REPIICA <repiica_one@REPIICAui-MacBookPro.local>
2021-05-12 16:55:22 +0900

    This is First Merge Commit in New Bigsur Macbook Test

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
+import { createStore } from "redux"; // data 를 넣는 곳
+
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
+
+// modify data 앱의 상태를 변경하여 수정하는 것
+const reducer = (state = 0, action) => {
+    return state;
+};
+
+// 변경된 앱의 state 가 저장되는 곳.
+const store = createStore(reducer);
+
+console.log(store.getState());
 
 let count = 0;
 
