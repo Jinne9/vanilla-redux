bd3d1a8db73d1571b7abdf1260a3f9d10291e182
REPIICA <repiica_one@REPIICAui-MacBookPro.local>
2021-05-13 17:03:31 +0900

    Re Code Redux with Nomardcoder

--- src/index.js
+++ src/index.js
@@ -1,34 +1,28 @@
-import { createStore } from "redux"; // data 를 넣는 곳
+import { combineReducers, createStore } from "redux"; // data 를 넣는 곳
 
-const add = document.getElementById("add");
-const minus = document.getElementById("minus");
-const number = document.querySelector("span");
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const ul = document.querySelector("ul");
 
-// modify data 앱의 상태를 변경하여 수정하는 것
-const reducer = (state = 0, action) => {
-    return state;
+// to do 리스트 요소 만들기
+const createToDo = (toDo) => {
+    // li 리스트 요소 만들기
+    const li = document.createElement("li");
+    // li 리스트에 텍스트(할일) 대입
+    li.innerText = toDo;
+    // 해당 요소는 ul에 자식으로 집어넣는다.
+    ul.appendChild(li);
 };
 
-// 변경된 앱의 state 가 저장되는 곳.
-const store = createStore(reducer);
-
-console.log(store.getState());
-
-let count = 0;
-
-const updateText = () => {
-    number.innerText = count;
+// 할일 제출
+const onSubmit = (e) => {
+    e.preventDefault();
+    // 할일 텍스트
+    const toDo = input.value;
+    // 제출 후 빈 값으로 초기화
+    input.value = "";
+    // 제출한 할일 텍스트로 리스트 생성ㅎ아기
+    createToDo(toDo);
 };
 
-const handleAdd = () => {
-    count = count + 1;
-    updateText();
-};
-
-const handleMinus = () => {
-    count = count - 1;
-    updateText();
-};
-
-add.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
+form.addEventListener("submit", onSubmit);
